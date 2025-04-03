14718645f43e505aba04d9088d9c7f0c3ec18b0b
MartijnMakaske <83974365+MartijnMakaske@users.noreply.github.com>
2025-04-03 16:10:58 +0200

    Further model development

--- model_training.py
+++ model_training.py
@@ -21,6 +21,7 @@ from torch.optim.lr_scheduler import StepLR
 
 from sklearn.model_selection import StratifiedKFold
 from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
+from sklearn.metrics import roc_auc_score
 
 #----------------------------------
 # CLASSES
@@ -64,7 +65,7 @@ class SiameseNetwork(nn.Module):
         self.base_model = base_model
         self.adaptive_pool = nn.AdaptiveAvgPool3d((1, 1, 1))
         self.classifier = nn.Sequential(
-            nn.Linear(2054,256),
+            nn.Linear(518,256),     #use 2054 for resnet50
             nn.ReLU(),
             nn.Linear(256,9)    # No softmax, because BCEWithLogitsLoss applies sigmoid internally
         )
@@ -90,32 +91,32 @@ class SiameseNetwork(nn.Module):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-data_dir = "L:/Basic/divi/jstoker/slicer_pdac/Master Students WS 24/Martijn/data/Training/paired_scans"
-clinical_data_dir = "C:/Users/P095550/OneDrive - Amsterdam UMC/Documenten/GitHub/CRLM-morph-features"
+data_dir = "/scratch/bmep/mfmakaske/training_scans/"
+clinical_data_dir = "/scratch/bmep/mfmakaske/"
 nifti_images = sorted(glob.glob(os.path.join(data_dir, "*.nii.gz")))   
 
 # Create pairs (e.g., first and second file are paired)
 image_pairs = [(nifti_images[i], nifti_images[i + 1]) for i in range(0, len(nifti_images) - 1, 2)]
 
 pd_metadata = pd.read_csv(os.path.join(clinical_data_dir, "training_input.csv"))
-metadata = torch.tensor(pd_metadata.values.tolist())
+all_metadata = torch.tensor(pd_metadata.values.tolist())
 
 pd_labels = pd.read_csv(os.path.join(clinical_data_dir, "training_labels.csv"))
-labels = torch.tensor(pd_labels.values.tolist()) #Fill in correct path. response, PFS, and OS
+all_labels = torch.tensor(pd_labels.values.tolist()) #Fill in correct path. response, PFS, and OS
 
 
 #------------------------------------
 # INITIALIZE ENCODER
 #------------------------------------
 
-resnet_model = torch.hub.load('Warvito/MedicalNet-models', 'medicalnet_resnet50')
+resnet_model = torch.hub.load('Warvito/MedicalNet-models', 'medicalnet_resnet10')
 
 # Remove the final classification layer (fc) to keep only the encoder part
 encoder = nn.Sequential(*list(resnet_model.children())[:-1])
 
 
 
-def train_model(model, train_loader, criterion, optimizer, scheduler, num_epochs=10, device="cuda"):
+def train_model(model, train_loader, val_loader, criterion, optimizer, scheduler, num_epochs=10, device="cuda"):
 
     for epoch in range(num_epochs):
         print(f"\nEpoch {epoch + 1}/{num_epochs}")
@@ -129,12 +130,12 @@ def train_model(model, train_loader, criterion, optimizer, scheduler, num_epochs
         correct = 0
         total = 0
         
-        for img_1, img_2, metadata, labels in tqdm(train_loader):
-            img_1, img_2, metadata, labels = img_1.to(device), img_2.to(device), metadata.to(device), labels.to(device)
+        for train_img_1, train_img_2, train_metadata, train_labels in tqdm(train_loader):
+            train_img_1, train_img_2, train_metadata, train_labels = train_img_1.to(device), train_img_2.to(device), train_metadata.to(device), train_labels.to(device)
             
             # Forward pass
-            outputs = model(img_1, img_2, metadata)
-            loss = criterion(outputs, labels)
+            outputs = model(train_img_1, train_img_2, train_metadata)
+            loss = criterion(outputs, train_labels)
             
             # Backward pass and optimization
             optimizer.zero_grad()
@@ -150,13 +151,14 @@ def train_model(model, train_loader, criterion, optimizer, scheduler, num_epochs
             preds = (probs > 0.5).float()
             
             # Calculate metrics
-            correct += (preds == labels).sum().item()
-            total += labels.numel()
+            correct += (preds == train_labels).sum().item()
+            total += train_labels.numel()
         
         epoch_loss = running_loss / len(train_loader)
         epoch_acc = correct / total
         
         print(f'Training complete. Train Loss: {epoch_loss:.4f} | Train Acc: {epoch_acc:.4f}')
+        validate_model(model, val_loader, criterion, device)
 
         # ---------------------------
         # LEARNING RATE SCHEDULER STEP
@@ -164,10 +166,88 @@ def train_model(model, train_loader, criterion, optimizer, scheduler, num_epochs
         scheduler.step()
 
 
+def validate_model(model, val_loader, criterion, device="cuda"):
+    model.eval()
+    global best_val_loss
+    val_loss = 0.0
+    correct = 0
+    total = 0
+
+    all_probs = []
+    all_ground_truths = []
+
+    with torch.no_grad():
+        for val_img_1, val_img_2, val_metadata, val_labels in val_loader:
+            val_img_1, val_img_2, val_metadata, val_labels = val_img_1.to(device), val_img_2.to(device), val_metadata.to(device), val_labels.to(device)
+            
+            # Forward pass
+            outputs = model(val_img_1, val_img_2, val_metadata)
+            loss = criterion(outputs, val_labels)
+            val_loss += loss.item()
+
+            # Apply sigmoid to get probabilities
+            probs = torch.sigmoid(outputs)
+
+            # Store predictions and labels for AUC-ROC calculation
+            all_probs.append(probs.cpu().numpy())
+            all_ground_truths.append(val_labels.cpu().numpy())
+
+            # Apply threshold to get binary predictions
+            preds = (probs > 0.5).float()
+
+            # Calculate metrics
+            correct += (preds == val_labels).sum().item()
+            total += val_labels.numel()
+
+    val_loss /= len(val_loader)
+    val_acc = correct / total
+
+    # Concatenate all predictions and labels
+    all_probs = np.vstack(all_probs)
+    all_ground_truths = np.vstack(all_ground_truths)
+
+    # Calculate AUC-ROC
+    try:
+        auc_roc = roc_auc_score(all_ground_truths, all_probs, average="macro")
+    except ValueError:
+        auc_roc = float('nan')  # Handle cases where AUC-ROC cannot be calculated
+
+    print(f"Validation Loss: {val_loss:.4f} | Validation Accuracy: {val_acc:.4f} | AUC-ROC: {auc_roc:.4f}")
+    metrics_per_outcome(all_ground_truths, all_probs)
+
+    # ---------------------------
+    # SAVE BEST MODEL
+    # ---------------------------
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        torch.save(model.state_dict(), 'best_model.pth')
+        print("Best model saved!")
+
+
+def metrics_per_outcome(all_ground_truths, all_probs):
+    outcomes = {"path_resp": 0, "PFS": 3, "OS": 6}
+    for (outcome, i) in outcomes.items():
+        # Calculate AUC-ROC
+        try:
+            auc_roc = roc_auc_score(all_ground_truths[:, i:i+3], all_probs[:, i:i+3], average="macro")
+        except ValueError:
+            auc_roc = float('nan')  # Handle cases where AUC-ROC cannot be calculated
+        print(f"{outcome} - AUC-ROC: {auc_roc}")
+
+
+
+
+
+
+
+
+
 # HYPERPARAMETERS
 n_splits = 5
-batch_size = 1
-num_epochs = 1
+batch_size = 4
+num_epochs = 50
+class_weights = torch.tensor([ 5.7600,  8.0000, 10.2857,  1.2152,  2.4202,  9.0000,  1.1803,  2.9091,
+                            12.0000], dtype=torch.float32).to(device)
 
 # Initialize KFold
 mlskf = MultilabelStratifiedKFold(n_splits=5, shuffle=True, random_state=42)
@@ -176,26 +256,18 @@ mlskf = MultilabelStratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 fold_results = []
 best_val_loss = float('inf')
 
-print(f"length image pairs: {len(image_pairs)}")
-print(f"length metadata: {len(metadata)}")
-print(f"Length labels: {len(labels)}")
-
 # Perform 5-fold cross-validation
-for fold, (train_idx, val_idx) in enumerate(mlskf.split(image_pairs, labels)):
+for fold, (train_idx, val_idx) in enumerate(mlskf.split(image_pairs, all_labels)):
     print(f"\nFold {fold + 1}/{n_splits}")
     print("-" * 20)
-
-    print(train_idx)
-    print(val_idx)
-
 
     # Split the data into training and validation sets for this fold
     train_image_pairs = [image_pairs[i] for i in train_idx]
     val_image_pairs = [image_pairs[i] for i in val_idx]
-    train_metadata = metadata[train_idx]
-    val_metadata = metadata[val_idx]
-    train_labels = labels[train_idx]
-    val_labels = labels[val_idx]
+    train_metadata = all_metadata[train_idx]
+    val_metadata = all_metadata[val_idx]
+    train_labels = all_labels[train_idx]
+    val_labels = all_labels[val_idx]
 
     # Create training and validation datasets
     train_dataset = PairedMedicalDataset(
@@ -214,7 +286,7 @@ for fold, (train_idx, val_idx) in enumerate(mlskf.split(image_pairs, labels)):
     model = model.to(device)
 
     # Loss function
-    criterion = nn.BCEWithLogitsLoss()
+    criterion = nn.BCEWithLogitsLoss(pos_weight=class_weights)
 
     # Optimizer
     optimizer = optim.Adam(model.parameters(), lr=1e-4)
@@ -223,35 +295,57 @@ for fold, (train_idx, val_idx) in enumerate(mlskf.split(image_pairs, labels)):
     scheduler = StepLR(optimizer, step_size=5, gamma=0.1)
 
     # Train the model for this fold
-    #train_model(model, train_loader, criterion, optimizer, scheduler, num_epochs=num_epochs, device=device)
+    train_model(model, train_loader, val_loader, criterion, optimizer, scheduler, num_epochs=num_epochs, device=device)
     """
     # Evaluate the model on the validation set
     val_loss = 0.0
     correct = 0
     total = 0
 
+    # Initialize lists to store predictions and ground truth
+    all_probs = []
+    all_ground_truths = []
+    
     model.eval()
     with torch.no_grad():
-        for img_1, img_2, metadata, labels in val_loader:
-            img_1, img_2, metadata, labels = img_1.to(device), img_2.to(device), metadata.to(device), labels.to(device)
-            outputs = model(img_1, img_2, metadata)
-            loss = criterion(outputs, labels)
+        for val_img_1, val_img_2, val_metadata, val_labels in val_loader:
+            val_img_1, val_img_2, val_metadata, val_labels = val_img_1.to(device), val_img_2.to(device), val_metadata.to(device), val_labels.to(device)
+            outputs = model(val_img_1, val_img_2, val_metadata)
+            loss = criterion(outputs, val_labels)
             val_loss += loss.item()
 
-            # Apply sigmoid and threshold
+            # Apply sigmoid
             probs = torch.sigmoid(outputs)
+
+            # Store predictions and labels for AUC-ROC calculation
+            all_probs.append(probs.cpu().numpy())
+            all_ground_truths.append(val_labels.cpu().numpy())
+
+            #apply threshold
             preds = (probs > 0.5).float()
 
             # Calculate metrics
-            correct += (preds == labels).sum().item()
-            total += labels.numel()
+            correct += (preds == val_labels).sum().item()
+            total += val_labels.numel()
 
     val_loss /= len(val_loader)
     val_acc = correct / total
-    print(f"Fold {fold + 1} - Val Loss: {val_loss:.4f} | Val Acc: {val_acc:.4f}")
 
+    # Concatenate all predictions and labels
+    all_probs = np.vstack(all_probs)
+    all_ground_truths = np.vstack(all_ground_truths)
+
+    # Calculate AUC-ROC
+    try:
+        auc_roc = roc_auc_score(all_ground_truths, all_probs, average="macro")
+    except ValueError:
+        auc_roc = float('nan')  # Handle cases where AUC-ROC cannot be calculated
+    
     # Store results for this fold
-    fold_results.append({"fold": fold + 1, "val_loss": val_loss, "val_acc": val_acc})
+    print(f"Fold {fold + 1} ---------------------------")
+    print(f"Val Loss: {val_loss:.4f} | Val Acc: {val_acc:.4f} | AUC-ROC: {auc_roc:.4f}")
+
+    metrics_per_outcome(all_ground_truths, all_probs)
 
     # ---------------------------
     # SAVE BEST MODEL
@@ -261,6 +355,7 @@ for fold, (train_idx, val_idx) in enumerate(mlskf.split(image_pairs, labels)):
         torch.save(model.state_dict(), 'best_model.pth')
         print("Best model saved!")
     """
+
 # Calculate average metrics across all folds
 avg_val_loss = sum([result["val_loss"] for result in fold_results]) / n_splits
 avg_val_acc = sum([result["val_acc"] for result in fold_results]) / n_splits
